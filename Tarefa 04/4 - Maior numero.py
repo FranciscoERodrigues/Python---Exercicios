499c67571bd91207e81ac7528fd7aa31ae5ff928
FranciscoERodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2024-05-25 09:13:37 -0300

    Update 4 - Maior numero.py

--- Tarefa 04/4 - Maior numero.py
+++ Tarefa 04/4 - Maior numero.py
@@ -3,12 +3,12 @@ valor2 = int(input("Digite o segundo valor:" ))
 valor3 = int(input("Digite o terceiro valor:" ))
 
 if(valor1>valor2 and valor1>valor3):
-    print("o", valor1, "é o maior numero entre os tres valores")
+    print("o valor", valor1, "é o maior numero entre os tres valores")
 
 if(valor2>valor1 and valor2>valor3):
-    print("o", valor2, "é o maior numero entre os tres valores")
+    print("o valor ", valor2, "é o maior numero entre os tres valores")
 
 if(valor3>valor1 and valor3>valor2):
-    print( "o", valor3, "é o maior numero entre os tres valores")
+    print( "o valor", valor3, "é o maior numero entre os tres valores")
 
 
